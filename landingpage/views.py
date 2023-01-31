389e47f32b3c2db696258eb887e864b8875119b9
vqton <vuquangton@outlook.com>
2023-01-31 15:49:25 +0700

    Implemenet images module

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImageForm
 
 # Create your views here.
 
 
 def home(request):
     return render(request, "landingpage\landingpage.html")
+
+
+def upload_image(request):
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("list_images")
+    else:
+        form = ImageForm()
+    return render(request, "upload.html", {"form": form})
